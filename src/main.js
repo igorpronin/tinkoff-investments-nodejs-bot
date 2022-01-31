4d06468a7a980a59fddb8781c300bba2c7bc396a
igor <legalsolut@ya.ru>
2022-01-31 21:09:51 +0300

    test cron

--- src/main.js
+++ src/main.js
@@ -20,6 +20,12 @@ const hourlyTask = cron.schedule('0 * * * *', () =>  {
   scheduled: false
 });
 
+const dailyDask = cron.schedule('0 11 * * *', () =>  {
+  sendDealsToTelegram().then();
+}, {
+  scheduled: false
+});
+
 const getDealMesAndSum = (i, deal) => {
   const {id, ticker, direction, trigger_price, order_type, order_price, lots, lot, is_executed, currency} = deal;
   const prefix = is_executed ? '[ИСПОЛНЕНА]' : '[АКТИВНА]  ';
@@ -434,7 +440,8 @@ const runMain = async () => {
   }
   sendDealsToTelegram().then();
   mondayTask.start();
-  hourlyTask.start();
+  // hourlyTask.start();
+  dailyDask.start();
 }
 
 module.exports = {runMain, noDealsMes, getDealMesAndSum};
